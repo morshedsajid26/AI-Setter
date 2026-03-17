977b5d3eb66fd0a4d22d7079699beb9715b8ee1c
morshedsajid26 <neazmorshed407@gmail.com>
2026-03-17 06:03:05 +0600

    updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://charissa-intuitable-corroboratorily.ngrok-free.dev';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://test19.fireai.agency';
